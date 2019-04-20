e619eb4679f8797feedfbda0b8e2614a992436f5
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-04-19 19:00:07 -0700

    wrapped code in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,51 @@
-var repository = []; // to hold list of Pokemon characters
-var bigWeight = 85;
+function executeProgram() {
+  var data = {};
 
-var bulbasaur = {
-  name: 'Bulbasaur',
-  height: 0.7,
-  weight: 6.9,
-  types: ['grass', 'poison']
-};
+  var repository = []; // to hold list of Pokemon characters
+  var bigWeight = 85;
 
-var blastoise = {
-  name: 'Blastoise',
-  height: 1.6,
-  weight: 85.5,
-  types: ['water']
-};
+  var bulbasaur = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    types: ['grass', 'poison']
+  };
 
-var spearow = {
-  name: 'Spearow',
-  height: 0.3,
-  weight: 2,
-  types: ['flying', 'normal']
-};
+  var blastoise = {
+    name: 'Blastoise',
+    height: 1.6,
+    weight: 85.5,
+    types: ['water']
+  };
 
-var nidoqueen = {
-  name: 'Nidoqueen',
-  height: 1.3,
-  weight: 60,
-  types: ['ground', 'poison']
-};
+  var spearow = {
+    name: 'Spearow',
+    height: 0.3,
+    weight: 2,
+    types: ['flying', 'normal']
+  };
 
-var repository = [bulbasaur, blastoise, spearow, nidoqueen];
+  var nidoqueen = {
+    name: 'Nidoqueen',
+    height: 1.3,
+    weight: 60,
+    types: ['ground', 'poison']
+  };
 
-/*
-print out the list of Pokemon names and weights, noting any with weight
-greater than 'bigWeight'
-*/
-for (var i = 0; i < repository.length; i++) {
-  var pokemonComment = repository[i].name + '(weight: ' + repository[i].weight + ' kg)';
+  var repository = [bulbasaur, blastoise, spearow, nidoqueen];
 
-  if (repository[i].weight > bigWeight) {
-    pokemonComment += ' - Wow, that\'s big!';
+  /*
+  print out the list of Pokemon names and weights, noting any with weight
+  greater than 'bigWeight'
+  */
+  for (var i = 0; i < repository.length; i++) {
+    var pokemonComment = repository[i].name + '(weight: ' + repository[i].weight + ' kg)';
+
+    if (repository[i].weight > bigWeight) {
+      pokemonComment += ' - Wow, that\'s big!';
+    }
+
+    document.write('<p>' + pokemonComment + '</p>');
   }
-
-  document.write('<p>' + pokemonComment + '</p>');
 }
+executeProgram();
