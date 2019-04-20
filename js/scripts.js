2e46c4ba3bb1277de0a82ff2f51523e08d52db62
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-04-19 19:39:51 -0700

    refactored, repository pattern

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,68 @@
 function executeProgram() {
   var data = {};
 
-  var repository = []; // to hold list of Pokemon characters
-  var bigWeight = 85;
+  var pokemonRepository = (function() {
 
-  var pikachu = {
-    name: 'Pikachu',
-    height: 0.4,
-    weight: 6.0,
-    types: ['electric']
-  };
+    var repository = []; // to hold list of Pokemon characters
+    var bigWeight = 85;
+
+    var pikachu = {
+      name: 'Pikachu',
+      height: 0.4,
+      weight: 6.0,
+      types: ['electric']
+    };
 
     var bulbasaur = {
-    name: 'Bulbasaur',
-    height: 0.7,
-    weight: 6.9,
-    types: ['grass', 'poison']
-  };
+      name: 'Bulbasaur',
+      height: 0.7,
+      weight: 6.9,
+      types: ['grass', 'poison']
+    };
 
-  var mew = {
-  name: 'Mew',
-  height: 0.4,
-  weight: 4.0,
-  types: ['psychic']
-  };
+    var mew = {
+      name: 'Mew',
+      height: 0.4,
+      weight: 4.0,
+      types: ['psychic']
+    };
 
-  var blastoise = {
-    name: 'Blastoise',
-    height: 1.6,
-    weight: 85.5,
-    types: ['water']
-  };
+    var blastoise = {
+      name: 'Blastoise',
+      height: 1.6,
+      weight: 85.5,
+      types: ['water']
+    };
 
-  var spearow = {
-    name: 'Spearow',
-    height: 0.3,
-    weight: 2,
-    types: ['flying', 'normal']
-  };
+    var spearow = {
+      name: 'Spearow',
+      height: 0.3,
+      weight: 2,
+      types: ['flying', 'normal']
+    };
 
-  var nidoqueen = {
-    name: 'Nidoqueen',
-    height: 1.3,
-    weight: 60,
-    types: ['ground', 'poison']
-  };
+    var nidoqueen = {
+      name: 'Nidoqueen',
+      height: 1.3,
+      weight: 60,
+      types: ['ground', 'poison']
+    };
 
-  var repository = [bulbasaur, blastoise, spearow, nidoqueen];
+    var repository = [pikachu, mew, bulbasaur, blastoise, spearow, nidoqueen];
 
+    function getAll() {
+      return repository;
+    }
+
+    function add(pokemon) {
+      repository.push(pokemon);
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
   /*
   print out the list of Pokemon names and weights, noting any with weight
   greater than 'bigWeight'
