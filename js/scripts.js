05cd5100b2c1021123593102d680f3ecfea17e9e
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-04-21 09:47:43 -0700

    added validation for matching keys when adding data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function executeProgram() { // repository wrapped in IIFE
+(function () { // repository wrapped in IIFE
   var data = {};
 
   var pokemonRepository = (function() {
@@ -49,12 +49,41 @@ function executeProgram() { // repository wrapped in IIFE
 
     var repository = [pikachu, mew, bulbasaur, blastoise, spearow, nidoqueen];
 
+    /* check for empty repository
+        return true if empty, false if not
+    */
+    function isRepositoryEmpty(repository) {
+      if (repository.length > 0) {
+        return false;
+      }
+      return true;
+    }
+
+    /* checks each key for two objects, returning true if all match, false
+        otherwise
+    */
+    function isKeyMatch(object1, object2) {
+      var object1Keys = Object.keys(object1);
+      var object2Keys = Object.keys(object2);
+
+      for (var i = 0; i < object1Keys.length; i++) {
+        if (object1Keys[i] !== object2Keys[i]) {
+          return false; // not the same key here
+      }
+      return true;  //if made it here, all keys match
+    }
+  }
+
     function getAll() {
       return repository;
     }
 
     function add(pokemon) {
-      repository.push(pokemon);
+      if ((typeof pokemon === 'object') && ((isRepositoryEmpty(repository) ||
+        (isKeyMatch(respository[0], pokemon))))){
+          repository.push(pokemon);
+        }
+      else console.warn("Not valid pokemon data, item not added!");
     }
 
     return {
@@ -67,7 +96,6 @@ function executeProgram() { // repository wrapped in IIFE
   print out the list of Pokemon names and weights, noting any with weight
   greater than 85
   */
-
   pokemonRepository.getAll().forEach(function(pokemon) {
     var pokemonComment = pokemon.name + ' (weight: ' + pokemon.weight + ' kg)';
 
@@ -77,5 +105,6 @@ function executeProgram() { // repository wrapped in IIFE
 
     document.write('<p>' + pokemonComment + '</p>');
   });
-}
-executeProgram();
+
+
+})();
