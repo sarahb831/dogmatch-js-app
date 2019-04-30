0080fa65ca89d6323d1fa4056673736fd0de0a47
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-04-29 20:35:41 -0700

    'prettier' updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-(function () { // IIFE wrapper
+(function() {
+  // IIFE wrapper
   var data = {};
 
   var $modalContainer = document.querySelector('#modal-container');
 
   var pokemonRepository = (function() {
-
     var repository = []; // to hold list of Pokemon characters
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
-
 
     /* check for empty repository
         return true if empty, false if not
@@ -29,57 +28,63 @@
       for (var i = 0; i < object1Keys.length; i++) {
         if (object1Keys[i] !== object2Keys[i]) {
           return false; // not the same key here
+        }
+        return true; //if made it here, all keys match
       }
-      return true;  //if made it here, all keys match
     }
-  }
 
-  /*
+    /*
   get the list of items from the api
   */
-  function loadList(){
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function(item) {
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function(e) {
-      console.error(e);
-    })
-  }
+    function loadList() {
+      return fetch(apiUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(json) {
+          json.results.forEach(function(item) {
+            var pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+        })
+        .catch(function(e) {
+          console.error(e);
+        });
+    }
 
-  /*
+    /*
   load the details for the specified item
   */
-  function loadDetails(item) {
-    var url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // now add details to the list__item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.weight = details.weight;
-      item.types = Object.keys(details.types);
-    }).catch(function(e) {
-      console.error(e);
-    });
-  }
+    function loadDetails(item) {
+      var url = item.detailsUrl;
+      return fetch(url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(details) {
+          // now add details to the list__item
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.weight = details.weight;
+          item.types = Object.keys(details.types);
+        })
+        .catch(function(e) {
+          console.error(e);
+        });
+    }
 
     /*
     returns the object from the repository for the given name value
     */
     function search(name) {
       for (var i = 0; i < repository.length; i++) {
-          if (repository[i].name === name) {
-            return repository[i];
-          }
+        if (repository[i].name === name) {
+          return repository[i];
         }
+      }
       return null; // if no match found
     }
 
@@ -91,11 +96,12 @@
     add item to repository
     */
     function add(pokemon) {
-      if ((typeof pokemon === 'object') && ((isRepositoryEmpty(repository) ||
-        (isKeyMatch(repository[0], pokemon))))){
-          repository.push(pokemon);
-        }
-      else console.warn("Not valid pokemon data, item not added!");
+      if (
+        typeof pokemon === 'object' &&
+        (isRepositoryEmpty(repository) || isKeyMatch(repository[0], pokemon))
+      ) {
+        repository.push(pokemon);
+      } else console.warn('Not valid pokemon data, item not added!');
     }
 
     return {
@@ -109,7 +115,7 @@
 
   pokemonRepository.loadList().then(function() {
     // now data is loaded
-    pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.getAll().forEach(function(pokemon) {
       addListItem(pokemon);
     });
   });
@@ -177,24 +183,26 @@
     button.innerText = pokemon.name;
     newLi.appendChild(button);
     $element.appendChild(newLi);
-    newLi.addEventListener('click', function(event){
+    newLi.addEventListener('click', function(event) {
       showDetails(event.target.innerText);
     });
   } // end addListItem()
 
   // exit modal gracefully on Escape key press
-  window.addEventListener('keydown',(e) => {
-    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+  window.addEventListener('keydown', e => {
+    if (
+      e.key === 'Escape' &&
+      $modalContainer.classList.contains('is-visible')
+    ) {
       hideModal();
     }
   });
 
-  $modalContainer.addEventListener('click',(e) => {
+  $modalContainer.addEventListener('click', e => {
     // only close if user clicks on overlay
     var target = e.target;
     if (target === $modalContainer) {
       hideModal();
     }
   });
-
 })();
